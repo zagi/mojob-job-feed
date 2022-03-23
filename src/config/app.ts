bf90bd63b423bb8c52c5d3e64d76cb4b7bae47e1
zagi <biuro@zagalski.pl>
2022-03-23 23:06:18 +0100

    Add default params for job listing request

--- src/config/app.ts
+++ src/config/app.ts
@@ -1,3 +1,7 @@
 export const FIVE_PER_PAGE: number = 5;
 export const TWENTYFIVE_PER_PAGE: number = 25;
 export const DISPLAY_ALL: number = -1;
+export const JOB_LISTING_DEFAULT_PARAMS: Object = {
+    include_open: false,
+    use_mojob_feed_filter: true
+};
