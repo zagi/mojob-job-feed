563b057727286f90b5d37f54d03d2088d49fa0f0
zagi <biuro@zagalski.pl>
2022-03-23 21:02:15 +0100

    Add initial for NItemsPerPage component

---
+++ src/components/NItemsPerPageDropdown.vue
@@ -0,0 +1,22 @@
+<template>
+  <v-menu bottom offset-y close-on-click>
+    <template v-slot:activator="{ on, attrs }">
+      <v-btn color="transparent" v-bind="attrs" v-on="on">
+        {{ itemsPerPage > 0 ? `${itemsPerPage} per page` : "Display all" }}
+        <v-icon>
+          mdi-menu-down
+        </v-icon>
+      </v-btn>
+    </template>
+    <v-list color="transparent"> </v-list>
+  </v-menu>
+</template>
+
+<script lang="ts">
+import { Component, Prop, Vue, Watch } from "vue-property-decorator";
+
+@Component
+export default class NItemsPerPageDropdown extends Vue {
+  itemsPerPage: number = 5;
+}
+</script>
