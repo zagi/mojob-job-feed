bf4e611f022ec97e21b3336154c5790a4a789544
zagi <biuro@zagalski.pl>
2022-03-23 19:00:52 +0100

    Fix text-align

--- src/App.vue
+++ src/App.vue
@@ -16,11 +16,12 @@
   font-family: Avenir, Muli, sans-serif !important;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
+  /* text-align: center; */
   color: #2c3e50;
 }
 #nav {
   padding: 30px;
+  text-align: center;
 }
 #nav a {
   font-weight: bold;
