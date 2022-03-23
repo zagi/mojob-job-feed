0084e7b4da483aa1046b0a208b0588d01ee233c1
zagi <biuro@zagalski.pl>
2022-03-23 18:32:58 +0100

    Add initial position function component

--- src/components/JobFeed.vue
+++ src/components/JobFeed.vue
@@ -1,12 +1,23 @@
 <template>
-  <div class="job-feed">INPUT JOB FEED DESIGN HERE</div>
+  <div class="job-feed">
+    <v-toolbar dense flat color="transparent" elevation="0">
+      <position-function-dropdown :position-functions="positionFunctions" />
+      <v-spacer></v-spacer>
+      Dropdown n items per page
+    </v-toolbar>
+  </div>
 </template>
 
 <script lang="ts">
 import { Component, Prop, Vue } from 'vue-property-decorator';
 import { JobListing, PositionFunction } from '@/models/models';
+import PositionFunctionDropdown from '@/components/PositionFunctionDropdown.vue';
 
-@Component
+@Component({
+  components: {
+    PositionFunctionDropdown,
+  },
+})
 export default class JobFeed extends Vue {
   @Prop({ default: () => [], type: Array as () => JobListing[] })
   private jobListings!: JobListing[];
---
+++ src/components/PositionFunctionDropdown.vue
@@ -0,0 +1,14 @@
+<template>
+  <div></div>
+</template>
+
+<script lang="ts">
+import { Component, Prop, Vue } from "vue-property-decorator";
+import { PositionFunction } from "@/models/models";
+
+@Component
+export default class PositionFunctionDropdown extends Vue {
+  @Prop({ default: () => [], type: Array as () => PositionFunction[] })
+  private positionFunctions!: PositionFunction[];
+}
+</script>
